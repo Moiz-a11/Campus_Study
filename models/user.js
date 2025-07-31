3d4d1bd85a829dbf070abb1cd0a9d6306bc3e288
Moiza-11 <moeez4286@gmail.com>
2025-08-01 01:00:32 +0530

    major update

---
+++ models/user.js
@@ -0,0 +1,29 @@
+
+const express= require("express");
+const mongoose = require("mongoose");
+
+async function main(){
+
+await mongoose.connect(('mongodb://127.0.0.1:27017/cs'))
+}
+main()
+.then(()=>{
+    console.log("connected");
+})
+const userSchema = new mongoose.Schema({
+
+    username :{
+type:String
+    },
+
+    email :{
+type:String
+    },
+
+
+
+})
+
+const User = mongoose.model("User",userSchema);
+
+module.exports = User;
